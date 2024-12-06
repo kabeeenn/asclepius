285f6d05668d4b1972d221da56f24974c4604b7e
kabeeenn <bintangimmanuela@gmail.com>
2024-12-06 16:02:04 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,7 +2,7 @@ const tf = require('@tensorflow/tfjs-node');
 
 async function loadModel() {
     try {
-        return await tf.loadGraphModel('https://storage.googleapis.com/asclepius-8080/submissions-model/model.json');
+        return await tf.loadGraphModel(process.env.MODEL_PATH);
     } catch (error) {
         console.error('Error loading model:', error);
         throw new Error('Failed to load model');
